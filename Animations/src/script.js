300de3e7819a14269d3f16fa90731bab5f49ded9
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-05-02 17:03:14 +0530

    animation using Three js

---
+++ Animations/src/script.js
@@ -0,0 +1,51 @@
+import './style.css'
+import * as THREE from 'three'
+import  gsap  from 'gsap'
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+const clock = new THREE.Clock();
+
+gsap.to(mesh.position,{duration:5, delay:1 , x:2})
+gsap.to(mesh.position,{duration:6, delay:6 , x:0})
+const tick = ()=>{
+
+    //time
+    // const elapsedTime = clock.getElapsedTime()
+    // //rotate mesh
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+    // mesh.position.z = Math.cos(elapsedTime)
+    //render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
